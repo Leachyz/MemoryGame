937b41776f143b6080da7c91103dd49d88b1065c
Ramoy <real.leach93@gmail.com>
2014-04-10 20:33:04 -0400

    added two player memory game updated

--- static/cards.js
+++ static/cards.js
@@ -1,31 +1,69 @@
 var soundElement = document.getElementById("sound");
 var supportStorage = false;
 var username = document.querySelector("#player_instruction");
-var userinfo = {"username": null, "turn": 24, "photo": null};
-var userturn = document.querySelector("#player_turns");
+var userinfo = {"username": "Player 1", "photo": null, "points": 0};
+/*var userturn = document.querySelector("#player_turns");*/
 var userpic = document.querySelector("#pic_instruction");
 var cardcontainer = document.querySelector("#sideBox");
 var num_click = 0;
-var numTurns = 24;
+/*var numTurns = 24;*/
 var toMatch = 8;
+var enemyinfo = {"username": "Player 2", "photo": null, "points": 0};
+/*var wrapper = document.querySelector("#sideBoxWrapper");*/
+var mode = 1;
+var user_turn = 0;
 
-function saveData(userinfo){
+
+function saveUserData(userinfo){
     if(supportStorage) 
         localStorage.player = JSON.stringify(userinfo);
 }
 
-if(typeof(Storage)!==undefined) {
-	supportStorage = true;
-    	if(localStorage.player === undefined){ 
-        	saveData(userinfo);
-	}else{
-        	userinfo = JSON.parse(localStorage.player);
-		numTurns = userinfo.turn;
-        	if(userinfo.username != null) 
-			username.innerHTML = "Hi, "+userinfo.username+"!";
-		if(userinfo.photo != null) userpic.setAttribute("src", userinfo.photo); 
+function saveEnemypponentData(enemyinfo){
+    if(supportStorage)
+        localStorage.enemy = JSON.stringify(enemyinfo);
+}
+
+/*wrapper.innerHTML="<div id='span'><div id='options'><input onclick='setPlayerMode(\"one\")'  type='button' value='Single Player' class='player_option'/><input onclick='setPlayerMode(\"two\")' type='button' value='VS Mode' class='player_option'/></div></div>";
+}*/
+
+function getGamePlayMode(mode) {
+	if(mode == 1) {
+		if(typeof(Storage)!==undefined) {
+			supportStorage = true;
+		    	if(localStorage.player === undefined){ 
+				saveData(userinfo);
+			}else{
+				userinfo = JSON.parse(localStorage.player);
+				numTurns = userinfo.turn;
+				if(userinfo.username != null) 
+					username.innerHTML = "Hi, "+userinfo.username+"!";
+				if(userinfo.photo != null) userpic.setAttribute("src", userinfo.photo);
+			} 
+		}
 	}
 }
+
+function twoPlayerMode(){    
+    if(mode == 2) {       
+        if(enemyinfo.username != null) document.querySelector("#enemy_profilename").innerHTML = "Hi, "+enemyinfo.username;
+        if(enemyinfo.photo != null) document.querySelector("#enemy_pic").setAttribute("src", enemyinfo.photo);
+        document.querySelector("#enemypoints").innerHTML = enemyinfo.points;
+        document.querySelector("#enemy_profilename").onclick=function(){
+            var usr = prompt("Please enter your username");
+            enemyinfo.username = usr;
+            saveEnemyInfo(enemyinfo);
+            if(usr != null) this.innerHTML = "Hi, "+usr;
+        }
+        document.querySelector("#enemy_pic").onclick=function(){
+            var photo = prompt("Enter url for profile image");
+            enemyinfo.photo = photo;
+            saveEnemyInfo(enemyinfo);
+            if(photo != null) document.querySelector("#enemy_photo").setAttribute("src", photo);
+        }    
+    }
+}
+
 function playSound(choice){
     if(choice == 1){ /*alert("OK");*/
         soundElement.setAttribute("src", "/static/sounds/turned.mp3");
@@ -69,6 +107,7 @@ userpic.onclick=function(){
 	saveData(userinfo);
 	if(uPhoto != null) document.querySelector("#player_photo").setAttribute("src", uPhoto);
 }
+
 document.querySelector("#save_game").onclick = function(){
 	if(supportStorage){
 /*console.log(cardcontainer.innerHTML);*/
@@ -80,6 +119,7 @@ document.querySelector("#save_game").onclick = function(){
 		alert("Local storage is not available. Try again later.");
 	}
 }
+
 function restart(){
 	userinfo.turn = 24;
 	localStorage.removeItem("gamedata");
@@ -92,12 +132,12 @@ document.querySelector("#reset_game").onclick =  function(){restart()};
 
 
 
-if (supportStorage && !isNaN(userinfo.turn)) {
+/*if (supportStorage && !isNaN(userinfo.turn)) {
 	var turn = parseInt(userinfo.turn);
 }else {
 	var turn = 24;
 }
-userturn.innerHTML = turn;
+userturn.innerHTML = turn;*/
 
 function shuffleCards(o){
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -202,7 +242,25 @@ var flip = function(ccard){
 	
 }
 
+function setPlayerMode(pmode){
+    mode = (pmode == 1)? 1 : (pmode == 2)? 2 : 1;    
+    if(mode == 2) document.querySelector("#player_info").innerHTML += "<p id='vs'>VS</p><div id='enemy'><p><span id='enemyName'>Player 2</span></p><p>Your points, <span id='enemyPoints'>0</span></p><div id='enemyPic'><img src='default_photo.png' id='enemyPhoto' /><p id='pic_instruction'>Click to change profile pic</p></div>";
+    if(supportStorage) localStorage.mode = mode;   
+    document.body.removeChild(holder);    
+    loadGameData();
+    document.querySelector("#gamer").style.display = "block";
+    startSession();
+    if(mode == 2) twoPlayerMode();    
+    document.querySelector("#profilename").onclick=function(){
+        var usr = prompt("Please enter your username");
+        userdata.username = usr;
+        saveData(userdata);
+        if(usr != null) this.innerHTML = "Hi, "+usr;
+    }
+}
+
 /*Example1: Name: Jestina, url for pic: https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-ash4/t1.0-9/1487362_10152778410190550_2848720520749792450_n.jpg */
 /*Example2: Name: Ramoy, url for pic: http://www.bubblews.com/assets/images/news/801720367_1393523485.jpg */
 /*Example3: Name: Kevin, url for pic: http://www.gospelreggae.com/images/albums/349.jpg */
+/*Example4: Name: Nicki, url for pic: http://heightnbra.com/wp-content/uploads/2014/01/Nicki-Minaj-Height.jpg note:not working, change later.*/
 
